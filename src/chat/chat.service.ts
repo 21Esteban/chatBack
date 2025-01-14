a9e47d41faa00c440c774fb65730f570fc44e6f3
21Esteban <jelc200521@gmail.com>
2025-01-14 08:33:23 -0500

    Agregar módulo de chat con soporte para TypeORM, implementar creación de chat y añadir campo de número de teléfono en DTO

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { CreateChatDto } from './dto/create-chat.dto';
 import { UpdateChatDto } from './dto/update-chat.dto';
+import { Chat } from './entities/chat.entity';
 
 @Injectable()
 export class ChatService {
   create(createChatDto: CreateChatDto) {
-    return 'This action adds a new chat';
+    const chat = new Chat();
+    console.log();
+    return chat;
   }
 
   findAll() {
